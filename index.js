44b0bc5f5e4a6cc849e6bc7d4a04d961df6ec637
Brahmahamadou <124585116+Brahmahamadou@users.noreply.github.com>
2024-01-25 15:41:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const inputCategorie = document.getElementById("categorie");
 const inputTitre = document.getElementById("titre");
 const inputDate = document.getElementById("date");
 const inputStatu = document.getElementById("statu");
-const inputDescription = document.getElementById("description")
+const inputDescription = document.getElementById("description");
 const inputTable = document.getElementById("table");
 const btnAjouter = document.getElementById("ajouter");
 const btnAjouter2 = document.getElementById("ajouter2");
@@ -27,12 +27,19 @@ const trtab = document.getElementById("valeur");
 const btnBouton1 = document.getElementById("bouton1");
 const btnBouton2 = document.getElementById("bouton2");
 const btnBouton3 = document.getElementById("bouton3");
-const tbody = document.getElementById("tbody");
-const notification = document.getElementById("notification")
-const notification2 = document.getElementById("notification2")
-const notification3 = document.getElementById("notification3")
-const floatingTextarea2 = document.getElementById("floatingTextarea2")
-const tableaudescripton = document.getElementById("tableaudescripton")
+const tbody = document.getElementById("tbody1");
+const tbodyDeux = document.getElementById("bod");
+const notification = document.getElementById("notification");
+const notification2 = document.getElementById("notification2");
+const notification3 = document.getElementById("notification3");
+const floatingTextarea2 = document.getElementById("floatingTextarea2");
+const tableaudescripton = document.getElementById("tableaudescripton");
+const btnti = document.getElementById("ti");
+const titre = document.getElementById("titre1");
+const pe = document.getElementById("pe");
+const pe1 = document.getElementById("pe1");
+
+
 // console.log(notification);
 let tabLocalStorage = [];
 if (!JSON.parse(localStorage.getItem("tabLocalStorage"))) {
@@ -88,8 +95,10 @@ function supprimer(index) {
 function modifier(index) {
   // Récupérer la tâche correspondant à l'index du tableau
   btnAjouter.classList.add("bu2")
-  btnAjouter2.classList.remove("bu2")
-
+  btnAjouter2.classList.remove("bu2");
+  titre.style.backgroundColor = '#06ffcb';
+  pe1.classList.add("hidden")
+  pe.classList.remove("hidden");
   // Remplir les champs d'entrée avec les valeurs de la tâche à modifier
   inputCategorie.value = tabLocalStorage[index].inputCategorie;
   inputTitre.value = tabLocalStorage[index].inputTitre;
@@ -99,8 +108,11 @@ function modifier(index) {
 
   btnAjouter2.addEventListener("click", function (event) {
     event.preventDefault();
+    titre.style.backgroundColor = '';
     btnAjouter.classList.remove("bu2");
     btnAjouter2.classList.add("bu2");
+    pe1.classList.remove("hidden");
+    pe.classList.add("hidden");
 
     tabLocalStorage[index].inputCategorie = inputCategorie.value /* nouvelle valeur */
     tabLocalStorage[index].inputTitre = inputTitre.value /* nouvelle valeur */
@@ -112,7 +124,7 @@ function modifier(index) {
     inputTitre.value = ""
     inputDate.value = ""
     inputDescription.value = ""
-    inputStatu.value = ""
+    inputStatu.value = "";
     notification3.classList.remove("hidden")
     setTimeout(() => {
       notification3.classList.add("hidden")
@@ -122,13 +134,36 @@ function modifier(index) {
     // Mettre à jour l'affichage
     todolist();
   })
+
   localStorage.setItem("tabLocalStorage", JSON.stringify(tabLocalStorage));
   todolist();
-}
+};
+
 //function pour afficher
 function afficher(index) {
 
-
+  tbodyDeux.innerHTML = `
+  <tr class="border-white">
+  <th scope="row">Date:</th>
+  <td>${tabLocalStorage[index].inputDate}</td>
+</tr>
+<tr class="border-white">
+  <th scope="row">Titre:</th>
+  <td>${tabLocalStorage[index].inputTitre}</td>
+</tr>
+<tr class="border-white">
+  <th scope="row">Categorie:</th>
+  <td>${tabLocalStorage[index].inputCategorie}</td>
+</tr>
+<tr class="border-white">
+  <th scope="row">Description:</th>
+  <td colspan="2">${tabLocalStorage[index].inputDescription}</td>
+</tr>
+<tr class="border-white">
+  <th scope="row">Statut:</th>
+  <td>${tabLocalStorage[index].inputStatu}</td>
+</tr>
+  `
   tableaudescripton.style.backgroundColor = '#ffffff'
   floatingTextarea2.style.backgroundColor = '#e3e9e9'
   body.style.backgroundColor = '#e3e9e9'
@@ -139,7 +174,6 @@ function afficher(index) {
     floatingTextarea2.style.backgroundColor = '';
     tableaudescripton.style.backgroundColor = ''
   }, 3000);
-
   todolist();
 }
 function todolist() {
@@ -149,7 +183,7 @@ function todolist() {
      <tr id="valeur" class="">
      <td>${index + 1}</td>
      <td>${element.inputDate}</td>
-     <td>${element.inputTitre}</td>
+     <td id="ti">${element.inputTitre}</td>
      <td>${element.inputCategorie} </td>
      <td class=" w-100 gap-2 d-flex justify-content-center align-items-center">
        <button id="bouton1" onclick="afficher(${index})"  class="border-0 oil d-flex justify-content-center align-items-center">
@@ -166,3 +200,8 @@ function todolist() {
   });
 }
 todolist();
+// Afficher Contenu Description
+btnti.addEventListener("click", function (e) {
+  e.preventDefault();
+  floatingTextarea2.textContent = inputTitre.value
+})
